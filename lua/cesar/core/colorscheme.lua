0b9030564396e5d787280b5bd4901f857d08fff7
numbers88s <cesar.r.jimenez@gmail.com>
2023-03-26 22:31:39 -0600

    config(theme): 💾  updating catppuccin setup

--- lua/cesar/core/colorscheme.lua
+++ lua/cesar/core/colorscheme.lua
@@ -5,9 +5,17 @@ if not catppuccin_status then
 end
 
 catppuccin.setup({
+	background = {
+		dark = "macchiato",
+	},
+	custom_highlights = {
+		NvimTreeNormal = { bg = "#262626" },
+		NvimTreeStatuslineNc = { fg = "#ffff2e" },
+	},
+	term_colors = true,
 	integrations = {
 		gitsigns = true,
-		nvimtree = false,
+		nvimtree = true,
 		telescope = false,
 		notify = true,
 		lsp_trouble = false,
